acfacb655b870f2dbadbcfec5dc075ccf5bee633
Fellipe Castro <fellipecastro27d@gmail.com>
2025-04-09 18:39:38 -0300

    API finalizada

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 import http from "http";
+import fs from "fs";
+import rotas from "./routes.js";
 
-const servidor = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-type", "text/plain; charset=utf-8");
-    res.end("Olá, Mundo!!!");
+fs.writeFile(
+    "./mensagem.txt",
+    "Olá, TIC em Trilhas do arquivo",
+    "utf-8",
+    (erro) => {
+        if (erro) {
+            console.log("Falha ao escrever o arquivo: ", erro);
+            return;
+        }
+        console.log("Arquivo foi criado com sucesso");
+    }
+);
+
+fs.readFile("./mensagem.txt", "utf-8", (erro, conteudo) => {
+    if (erro) {
+        console.log("Falha na leitura o arquivo: ", erro);
+        return;
+    }
+    console.log(`Conteúdo: ${conteudo}`);
+
+    iniciaServidorHttp(conteudo);
 });
 
-const porta = 3000;
-const host = "localhost";
+function iniciaServidorHttp(mensagem) {
+    const servidor = http.createServer((req, res) => {
+        rotas(req, res, mensagem);
+    });
 
-servidor.listen(porta, host, () => {
-    console.log(`Servidor executando em http://${host}:${porta}`);
-});
+    const porta = 3000;
+    const host = "localhost";
+
+    servidor.listen(porta, host, () => {
+        console.log(`Servidor executando em http://${host}:${porta}`);
+    });
+}
