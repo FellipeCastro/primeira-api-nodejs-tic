a8b8a6ffbd62a40b74aafc9b001f10a25c21c0e0
Fellipe Castro <fellipecastro27d@gmail.com>
2025-04-12 16:52:50 -0300

    API finalizada

--- app.js
+++ app.js
@@ -1,59 +1,31 @@
-import http from "http";
-import fs from "fs";
-import rotas from "./routes.js";
 import sqlite3 from "sqlite3";
-import { criaPedido, lerPedidos, sequelize } from "./models.js";
+import express from "express";
+import bodyParser from "body-parser";
+import { sequelize } from "./models.js";
+import { rotasProduto } from "./routes/produtos.js";
+import { rotasPedido } from "./routes/pedidos.js";
 
-const db = new sqlite3.Database("./tic.db", (erro) => {
-    if (erro) {
-        console.log("Falha ao inicializar banco de dados: ", erro);
-        return;
-    }
-    console.log("Banco de dados inicializado");
-});
+const app = express();
 
-fs.writeFile(
-    "./mensagem.txt",
-    "Olá, TIC em Trilhas do arquivo",
-    "utf-8",
-    (erro) => {
+app.use(bodyParser.json());
+
+app.use(rotasProduto);
+app.use(rotasPedido);
+
+async function inicializaApp() {
+    const db = new sqlite3.Database("./tic.db", (erro) => {
         if (erro) {
-            console.log("Falha ao escrever o arquivo: ", erro);
+            console.log("Falha ao inicializar banco de dados: ", erro);
             return;
         }
-        console.log("Arquivo foi criado com sucesso");
-    }
-);
+        console.log("Banco de dados inicializado");
+    });
 
-fs.readFile("./mensagem.txt", "utf-8", (erro, conteudo) => {
-    if (erro) {
-        console.log("Falha na leitura o arquivo: ", erro);
-        return;
-    }
-    console.log(`Conteúdo: ${conteudo}`);
-
-    iniciaServidorHttp(conteudo);
-});
-
-async function iniciaServidorHttp(mensagem) {
     await sequelize.sync();
-    await criaPedido({
-        valorTotal: 13.0,
-        produtos: [
-            { id: 2, quantidade: 1 },
-            { id: 4, quantidade: 2 },
-        ]
-    });
-    await lerPedidos();
-
-    const servidor = http.createServer((req, res) => {
-        rotas(req, res, mensagem);
-    });
 
     const porta = 3000;
-    const host = "localhost";
 
-    servidor.listen(porta, host, () => {
-        console.log(`Servidor executando em http://${host}:${porta}`);
-    });
+    app.listen(porta)
 }
+
+inicializaApp();
---
+++ routes/pedidos.js
@@ -0,0 +1,98 @@
+import express from "express";
+import { criaPedido, lerPedidoPorId, lerPedidos } from "../models.js";
+
+export const rotasPedido = express.Router();
+
+rotasPedido.post("/pedidos", async (req, res, next) => {
+    const pedido = req.body;
+
+    res.statusCode = 400;
+
+    if (!pedido?.produtos || !pedido.produtos.length) {
+        const resposta = {
+            erro: {
+                mensagem: `O atributo 'produtos' não foi encontrado ou está vazio, porém é obrigatório para a criação do pedido.`,
+            },
+        };
+        return res.send(resposta);
+    }
+
+    if (!pedido?.valorTotal || pedido.valorTotal <= 0) {
+        const resposta = {
+            erro: {
+                mensagem: `O atributo 'valorTotal' não foi encontrado ou é menor ou igual a zero, porém é obrigatório para a criação do pedido.`,
+            },
+        };
+        return res.send(resposta);
+    }
+
+    try {
+        const resposta = await criaPedido(pedido);
+        res.status(201).send(resposta);
+    } catch (erro) {
+        console.log("Falha ao criar pedido: ", erro);
+
+        const resposta = {
+            erro: {
+                mensagem: "Falha ao criar pedido",
+            },
+        };
+        res.status(500).send(resposta);
+    }
+});
+rotasPedido.get("/pedidos/:id", async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        const resposta = await lerPedidoPorId(id);
+
+        res.statusCode = 200;
+
+        if (!resposta) {
+            res.statusCode = 404;
+        }
+
+        res.send(resposta);
+
+        return;
+    } catch (erro) {
+        console.log("Falha ao buscar pedido: ", erro);
+
+        res.statusCode == 500;
+
+        const resposta = {
+            erro: {
+                mensagem: `Falha ao buscar pedido ${id}`,
+            },
+        };
+
+        res.send(resposta);
+
+        return;
+    }
+});
+rotasPedido.get("/pedidos", async (req, res, next) => {
+    try {
+        const resposta = await lerPedidos();
+
+        res.statusCode = 200;
+
+        res.send(resposta);
+
+        return;
+    } catch (erro) {
+        console.log("Falha ao buscar pedidos: ", erro);
+
+        res.statusCode == 500;
+
+        const resposta = {
+            erro: {
+                mensagem: `Falha ao buscar pedidos`,
+            },
+        };
+
+        res.send(resposta);
+
+        return;
+    }
+});
